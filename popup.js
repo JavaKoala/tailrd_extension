39559583f6ae24e02756191ba41dc4b29e4ee119
JavaKoala <javakoala1@gmail.com>
2025-02-08 16:22:58 -0500

    Randomize button

--- popup.js
+++ popup.js
@@ -1,3 +1,10 @@
+document.querySelector('#check-fitting').addEventListener('click', function(e) {
+  const options = ["It fits perfectly!", "It's a bit tight", "It's a bit loose", "It's too tight", "It's too loose"];
+  const fittingText = document.getElementById("fitting-text");
+  const option = options[Math.floor(Math.random() * options.length)];
+  fittingText.innerHTML = option;
+});
+
 document.querySelector('#fitting').addEventListener('click', function(e) {
   const popover = document.getElementById("fitting");
   popover.hidePopover();
