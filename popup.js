80fc519db19407dbb0ba981bc39a1764f9045dc6
JavaKoala <javakoala1@gmail.com>
2025-02-08 18:47:08 -0500

    Update fit messages

--- popup.js
+++ popup.js
@@ -1,5 +1,5 @@
 document.querySelector('#check-fitting').addEventListener('click', function(e) {
-  const options = ["It fits perfectly!", "It's a bit tight", "It's a bit loose", "It's too tight", "It's too loose"];
+  const options = ["It fits perfectly!", "It may be a bit tight<br>Try a larger size", "It may be a bit loose<br>Try a smaller size"];
   const fittingText = document.getElementById("fitting-text");
   const option = options[Math.floor(Math.random() * options.length)];
   fittingText.innerHTML = option;
